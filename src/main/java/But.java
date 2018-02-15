07e8d5eb2a69ebdd0e987291a3b87bd162a15a6a
Himuraa <36517830+Himuraa@users.noreply.github.com>
2018-02-15 22:48:33 +0300

    Initial commit

---
+++ src/main/java/But.java
@@ -0,0 +1,54 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class But extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init(); // Инициализируем апи
+        TelegramBotsApi botapi = new TelegramBotsApi();
+        try {
+            botapi.registerBot(new But());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "USER";
+        //возвращаем юзера
+    }
+
+    @Override
+    public void onUpdateReceived(Update e) {
+        Message msg = e.getMessage(); // Это нам понадобится
+        String txt = msg.getText();
+        if (txt.equals("/start")) {
+            sendMsg(msg, "Hello, world! This is simple bot!");
+        }
+        // Тут будет то, что выполняется при получении сообщения
+    }
+
+    @Override
+    public String getBotToken() {
+        return "YOUR_BOT_TOKEN";
+        //Токен бота
+    }
+
+    @SuppressWarnings("deprecation") // Означает то, что в новых версиях метод уберут или заменят
+    private void sendMsg(Message msg, String text) {
+        SendMessage s = new SendMessage();
+        s.setChatId(msg.getChatId()); // Боту может писать не один человек, и поэтому чтобы отправить сообщение, грубо говоря нужно узнать куда его отправлять
+        s.setText(text);
+        try { //Чтобы не крашнулась программа при вылете Exception
+            sendMessage(s);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
